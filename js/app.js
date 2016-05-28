e17694fa0267509d30dad9e8c953d5c6de0c45b7
Hugo Moragues <hugo.moragues@gmail.com>
2016-05-28 19:23:51 +0200

    varios
    se añaden fondos y codigo al js

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ function cargar_respuestas_ajax(xml, r) {
 			url: xml,
 			type: 'GET',
 			dataType: 'xml',
+			async: false,
 			success: function(data) {
 			//var xmlDoc = $.parseXML(data);
 			$xml = $(data);
@@ -23,7 +24,7 @@ function cargar_respuestas_ajax(xml, r) {
 			for (var i = 0; i < $respuestas.length; i++) {
 				r.push($respuestas[i].textContent);
 			}
-			return r;
+			
 		}
 	})
 		.done(function() {
@@ -34,6 +35,7 @@ function cargar_respuestas_ajax(xml, r) {
 		})
 		.always(function() {
 			console.log("complete");
+			return r;
 		});
 
 	}
@@ -41,10 +43,11 @@ function generar_bloques_preguntas($preguntas, destino) {
 	if($preguntas.length > 0) {
 		var result = "";
 		for(var i = 0; i < $preguntas.length; i++ ) {
-			result += "<p> Pregunta" + (i+1) + "<br />" + $preguntas[i].childNodes[1].textContent + "</p>";
-			result += "<ul><li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + $preguntas[i].childNodes[3].textContent + "</label></li>";
-			result += "<li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + $preguntas[i].childNodes[5].textContent + "</label></li>";
-			result += "<li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + $preguntas[i].childNodes[7].textContent + "</label></li></ul>";
+			result += "<h3> Pregunta" + (i+1) + "</h3>"
+			result += "<h4>" + $preguntas[i].childNodes[1].textContent + "</h4>";
+			result += "<ul class='.opcion'><li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + "<span class='opcion'>" + $preguntas[i].childNodes[3].textContent + "</span></label></li>";
+			result += "<li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + "<span class='opcion'>" + $preguntas[i].childNodes[5].textContent + "</span></label></li>";
+			result += "<li><label><input type='radio' name='"+i+"' value='"+(i+1)+"'>" + "<span class='opcion'>" + $preguntas[i].childNodes[7].textContent + "</span></label></li></ul>";
 		}
 		return result;
 	}
@@ -85,19 +88,39 @@ function get_respuestas(origen) {
 			break;
 		}
 	}
+	function marcar(str, indice) {
+		var selector = ".opcion:checked:eq("+indice+")";
+		switch (str) {
+			case 'verde':
+				$(selector).css({
+					color: 'green',
+				});
+				break;
+			case 'rojo':
+			$(selector).css({
+					color: 'red',
+				});
+				// statements_1
+				break;
+			default:
+				// statements_def
+				break;
+		}
+	}
 
 	function corregir_test() {
-		var resul = 0;
+		var result = 0;
 		var respuestas_usuario = get_respuestas("local");
 		var respuestas_original = get_respuestas("remoto");
 		for(var i = 0; i < respuestas_original.length; i++) {
 			if(comprobar_respuesta(respuestas_usuario[i], respuestas_original[i])) {
-				resul++;
+				result++;
+				marcar('verde', i+1);
 			} else {
-
+				marcar('rojo', i+1);
 			}
 		}
-		if(resul > 1) 
+		if(result > 1) 
 			mostrar_mensaje("#resultado", "<h2>Aprobado!</h2>");
 		else
 			mostrar_mensaje("#resultado", "<h2>Suspendido!</h2>");
@@ -151,6 +174,7 @@ function get_respuestas(origen) {
 
 
 	$(document).ready(function() {
+		$("#corregir, #anterior, #siguiente").hide();
 		$("#inicio").click(function(event) {
 			$("section").empty();
 			mostrar_mensaje("#resultado","<h1>Vamos, suerte!</h1>");
@@ -159,11 +183,16 @@ function get_respuestas(origen) {
 				display: 'none',
 			});
 			$("#corregir").css({
-				display: 'inline',
+				display: 'block',
 			});
-			
+			$("input").prop({
+				disabled: false,
+			});
 		});
 		$("#corregir").click(function(event) {
+			$("input").prop({
+				disabled: true,
+			});
 
 			mostrar_mensaje("#resultado","<h1>Correción de test</h1>");
 			corregir_test();
@@ -173,7 +202,7 @@ function get_respuestas(origen) {
 				display: 'none',
 			});
 			$("#inicio").css({
-				display: 'inline',
+				display: 'block',
 			});
 			
 
